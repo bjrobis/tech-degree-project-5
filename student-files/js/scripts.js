663d8327fae955fc632a2f67762c5de71ed286e9
Brandon_Robison <bjrobis@gmail.com>
2022-12-12 23:43:13 -0700

    Update scripts.js

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -21,6 +21,7 @@ function fetchData(url) {
   fetchData('https://randomuser.me/api/?results=12')
     .then(data => {
         let info = data.results;
+    
        for (let i = 0; i < info.length; i++) {
         let image = info[i].picture.thumbnail;
         let firstName = info[i].name.first;
@@ -28,54 +29,78 @@ function fetchData(url) {
         let email = info[i].email;
         let city = info[i].location.city;
         let cellPhone = info[i].cell;
-        let street = info[i].location.street;
+        let streetNumber = info[i].location.street.number;
+        let streetName = info[i].location.street.name;
         let state = info[i].location.state;
         let zip = info[i].location.postcode;
-        let dob = info[i].dob;
-        //The HTML that will be inserted into the gallery section
-        let galleryHTML = `
-        <div class="card">
-        <div class="card-img-container">
-        <img class="card-img" src="${image}" alt="profile picture">
-        </div>
-        <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-        <p class="card-text">${email}</p>
-        <p class="card-text cap">${city}, ${state}</p>
-        </div>
-        </div>
-        `;
-        insertHTML(gallery, galleryHTML);
-
-        //The HTML that will be inserted into the modal section
-        let modalHTML = `
-        <div class="modal-container">
-        <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-        <img class="modal-img" src="${image}" alt="profile picture">
-        <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
-        <p class="modal-text">${email}</p>
-        <p class="modal-text cap">${city}</p>
-        <hr>
-        <p class="modal-text">${cellPhone}</p>
-        <p class="modal-text">123 ${street}, ${city}, ${state} ${zip}</p>
-        <p class="modal-text">Birthday: ${dob}</p>
-        </div>
-        </div>
-        `
+        let dob = info[i].dob.date;
+        let birthMonth = dob.substring(5, 7);
+        let birthYear = dob.substring(0, 4);
+        let birthDay = dob.substring(8, 10);
+        let mmddyyyy = `${birthMonth}/${birthDay}/${birthYear}`;
         
-        //selects all contact cards
-         let cards = document.querySelectorAll('.card');
-         
-         cards.forEach(card => {
-              card.addEventListener('click', (e) => {
-              insertHTML(body, modalHTML);
-              });
-         });
+        //The HTML that will be inserted into the gallery section
+         let galleryHTML = `
+         <div class="card">
+         <div class="card-img-container">
+         <img class="card-img" src="${image}" alt="profile picture">
+         </div>
+         <div class="card-info-container">
+         <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+         <p class="card-text">${email}</p>
+         <p class="card-text cap">${city}, ${state}</p>
+         </div>
+         </div>
+         `;
+         insertHTML(gallery, galleryHTML);
+        
+
+          //The HTML that will be inserted into the modal section
+          let modalHTML = `
+          <div class="modal-container">
+          <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+          <img class="modal-img" src="${image}" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
+          <p class="modal-text">${email}</p>
+          <p class="modal-text cap">${city}</p>
+          <hr>
+          <p class="modal-text">${cellPhone}</p>
+          <p class="modal-text">${streetNumber} ${streetName}, ${city}, ${state} ${zip}</p>
+          <p class="modal-text">Birthday: ${mmddyyyy}</p>
+          </div>
+          </div>
+          `;
+          insertHTML(body, modalHTML);
+
     }
-     
+    let modalClose = document.querySelectorAll('.modal-close-btn');
+    let modalCards = document.querySelectorAll('.modal-container');
+    let contactCard = document.querySelectorAll('.card');
+    
+    //hides each modal card upon page load
+    modalCards.forEach(card => {
+        card.style.visibility = 'hidden';
+    });
+
+   //event listener for close button on modal
+    modalClose.forEach(button => {
+        button.addEventListener('click', (e) => {
+            e.target.parentNode.parentNode.parentNode.style.visibility = 'hidden';
+        });
+    })
+
+    //event listener for each contact cards
+    for (let i = 0; i < contactCard.length; i++) {
+        contactCard[i].addEventListener('click', (e) => {
+            modalCards[i].style.visibility = 'visible';
+        });
+    };
+
 })
+
+
 
 //***Search Bar
 
